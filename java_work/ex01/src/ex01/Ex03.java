25f5c34003e8dd29929e80f700bf8baf2f0c923b
hein425 <haein0425@google.com>
2024-06-28 12:58:49 +0900

    0628

---
+++ java_work/ex01/src/ex01/Ex03.java
@@ -0,0 +1,21 @@
+package ex01;
+
+/* 여러줄 주석처리
+ * 그대 나으 ㅣ작은 심장에 귀 기울 일때
+ * 입을 꼭 맞 추어 내 숨을 가져 가도 되요 
+ * 저무는아침에속삭이는숨
+ * 
+ * 창닫기 ctrl+w 
+ * 
+ */
+public class Ex03 {
+	public static void main(String[] args) {
+		System.out.println("10+3 = " + (10 + 3));
+		System.out.println("50/3 = " + 50 / 3);
+		System.out.println("50%3 = " + 50 % 3);
+		System.out.println("3*4 = " + 3 * 4);
+		System.out.println("10-7 = " + (10 - 7));
+
+	}
+
+}
