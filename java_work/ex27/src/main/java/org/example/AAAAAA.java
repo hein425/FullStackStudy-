074b75ad7a15be026400a3514dabe9d72d486dee
hein425 <haein0425@google.com>
2024-08-23 18:05:37 +0900

    0823

---
+++ java_work/ex27/src/main/java/org/example/AAAAAA.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class AAAAAA {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println( a + "*" + i + "=" + (a*i));
+        }
+//        for (int i = 1; i <= 5 ; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
+    }
+}
