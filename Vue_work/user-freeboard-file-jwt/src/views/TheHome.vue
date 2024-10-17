37d853073db9881eb68d6d68d2f29b97fbfda0f0
haein <heinnn425@gmail.com>
2024-10-17 17:43:55 +0900

    1017

--- Vue_work/user-freeboard-file-jwt/src/views/TheHome.vue
+++ Vue_work/user-freeboard-file-jwt/src/views/TheHome.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1 class="p-5 text-amber-500 text-5xl"> Hello :) </h1>
+    <p>Docker yea.</p>
   </div>
 </template>
 
---
+++ Vue_work/user-todo-dayjs/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+  <header class="flex space-x-10">
+    <RouterLink to="/">HOME</RouterLink>
+    <RouterLink to="/month">MONTH</RouterLink>
+    <RouterLink to="/about">ABOUT</RouterLink>
+  </header>
+  <RouterView />
+</template>
+<style scoped>
+</style>
